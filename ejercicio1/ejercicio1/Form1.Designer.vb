81bcb1d893a689bc978275ce84b4d6988decb18a
Yossec SA <yosec.arratea22@gmail.com>
2023-01-02 12:44:50 -0500

    ejercicio 1

---
+++ ejercicio1/ejercicio1/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPalabra = New System.Windows.Forms.TextBox()
+        Me.btnRegistrar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.listVocal = New System.Windows.Forms.ListBox()
+        Me.lisConso = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 56)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(139, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese palabra:"
+        '
+        'txtPalabra
+        '
+        Me.txtPalabra.Location = New System.Drawing.Point(174, 56)
+        Me.txtPalabra.Name = "txtPalabra"
+        Me.txtPalabra.Size = New System.Drawing.Size(138, 28)
+        Me.txtPalabra.TabIndex = 1
+        '
+        'btnRegistrar
+        '
+        Me.btnRegistrar.Location = New System.Drawing.Point(346, 52)
+        Me.btnRegistrar.Name = "btnRegistrar"
+        Me.btnRegistrar.Size = New System.Drawing.Size(99, 28)
+        Me.btnRegistrar.TabIndex = 2
+        Me.btnRegistrar.Text = "Registrar"
+        Me.btnRegistrar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 119)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(170, 21)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Comienza con vocal"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(243, 119)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(221, 21)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Comienza con consonante"
+        '
+        'listVocal
+        '
+        Me.listVocal.FormattingEnabled = True
+        Me.listVocal.ItemHeight = 21
+        Me.listVocal.Location = New System.Drawing.Point(20, 152)
+        Me.listVocal.Name = "listVocal"
+        Me.listVocal.Size = New System.Drawing.Size(184, 214)
+        Me.listVocal.TabIndex = 5
+        '
+        'lisConso
+        '
+        Me.lisConso.FormattingEnabled = True
+        Me.lisConso.ItemHeight = 21
+        Me.lisConso.Location = New System.Drawing.Point(247, 152)
+        Me.lisConso.Name = "lisConso"
+        Me.lisConso.Size = New System.Drawing.Size(191, 214)
+        Me.lisConso.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(469, 394)
+        Me.Controls.Add(Me.lisConso)
+        Me.Controls.Add(Me.listVocal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnRegistrar)
+        Me.Controls.Add(Me.txtPalabra)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Arial", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPalabra As TextBox
+    Friend WithEvents btnRegistrar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents listVocal As ListBox
+    Friend WithEvents lisConso As ListBox
+End Class
---
+++ ejercicio1/ejercicio1/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Drawing.Text
+Imports System.Security.Cryptography.X509Certificates
+
+Public Class Form1
+
+
+    Private Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
+
+        ingresarPalabra(txtPalabra.Text)
+
+    End Sub
+
+    Private Sub ingresarPalabra(palabra As String)
+
+
+        If "aeiou".Contains(palabra.ToLower().Chars(0)) Then
+
+            'para agregar palabra a listBox
+            listVocal.Items.Add(palabra)
+        Else
+            lisConso.Items.Add(palabra)
+        End If
+
+
+    End Sub
+End Class
